09f06febc206edf4c44d0840911e60f077531f57
Haakon <haakon@kindem.nu>
2020-01-24 00:58:05 +0100

    Completed python exercise with comments

--- Part2/python/foo.py
+++ Part2/python/foo.py
@@ -2,20 +2,30 @@
 # python fo.py
 
 from threading import Thread
+from threading import Lock
 
 # Potentially useful thing:
 #   In Python you "import" a global variable, instead of "export"ing it when you declare it
 #   (This is probably an effort to make you feel bad about typing the word "global")
 i = 0
+hog = Lock() #lock can be acquired by any thread, but one one at a time. any thread can at any time release lock. https://docs.python.org/3/library/threading.html
+
 
 def incrementingFunction():
     global i
     # TODO: increment i 1_000_000 times
+    for s in range (0, 99999):
+        hog.acquire()
+        i+= 1
+        hog.release()
 
 def decrementingFunction():
     global i
     # TODO: decrement i 1_000_000 times
-
+    for j in range (0, 99999):
+        hog.acquire()
+        i-= 1
+        hog.release()
 
 
 def main():
@@ -25,6 +35,8 @@ def main():
     decrementing = Thread(target = decrementingFunction, args = (),)
     
     # TODO: Start both threads
+    incrementing.start()
+    decrementing.start()
     
     incrementing.join()
     decrementing.join()
